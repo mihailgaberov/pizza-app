0c1140747b64af2a75a486702bca8b7e2a06cda5
Mihail Gaberov <mihail.gaberov@gmail.com>
2020-08-29 14:40:21 +0300

    :fire: Removed redundant boilerplate.

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import BootstrapVue from 'bootstrap-vue'
+
+Vue.use(BootstrapVue);
 
 Vue.config.productionTip = false
 
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    Home
   </div>
 </template>
 
