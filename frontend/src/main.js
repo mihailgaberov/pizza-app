92a4738de345ee18a2840ad5295374927cc3d33b
Mihail Gaberov <mihail.gaberov@gmail.com>
2020-08-29 14:11:07 +0300

    :tada: Installed Vue v.2

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,12 @@
-import { createApp } from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
 import store from './store'
 
-createApp(App).use(store).mount('#app')
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
--- frontend/tests/unit/example.spec.js
+++ frontend/tests/unit/example.spec.js
@@ -5,7 +5,7 @@ describe('HelloWorld.vue', () => {
   it('renders props.msg when passed', () => {
     const msg = 'new message'
     const wrapper = shallowMount(HelloWorld, {
-      props: { msg }
+      propsData: { msg }
     })
     expect(wrapper.text()).toMatch(msg)
   })
