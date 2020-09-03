d5f06ac08ac90d4e83e703e209a1f908b430e0a1
mihailgaberov <mihail.gaberov@gmail.com>
2020-09-03 11:52:53 +0300

    :sparkles: Updating Dashboard layout - added buttons and a nice pizza image.

--- frontend/src/components/Dashboard.vue
+++ frontend/src/components/Dashboard.vue
@@ -1,12 +1,15 @@
 <template>
-<div>
-  Love pizza button and clicks counter here
-</div>
+  <div id="dashboard">
+    <h1 class="h1">Love Pizza, Vote for Pizza</h1>
+    <b-btn type="submit" variant="success">Like +</b-btn>
+    <img src="../assets/pizza.png" alt="Love Pizza, Vote for Pizza" />
+    <b-btn type="submit" variant="danger">Dislike -</b-btn>
+  </div>
 </template>
 
 <script>
 export default {
-name: "Dashboard"
+  name: "Dashboard"
 }
 </script>
 
