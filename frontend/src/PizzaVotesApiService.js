60b824f68dac3423e6223aad169b99b215b6956b
mihailgaberov <mihail.gaberov@gmail.com>
2020-09-03 11:03:05 +0300

    :sparkles: Added a service to work with the API.

---
+++ frontend/src/PizzaVotesApiService.js
@@ -0,0 +1,32 @@
+import Vue from 'vue'
+import axios from 'axios'
+
+const client = axios.create({
+    baseURL: 'http://localhost:5000/api/pizzavotes',
+    json: true
+})
+
+export default {
+    async execute(method, resource, data) {
+        const accessToken = await Vue.prototype.$auth.getAccessToken()
+        return client({
+            method,
+            url: resource,
+            data,
+            headers: {
+                Authorization: `Bearer ${accessToken}`
+            }
+        }).then(req => {
+            return req.data
+        })
+    },
+    getAll() {
+        return this.execute('get', '/')
+    },
+    create(data) {
+        return this.execute('post', '/', data)
+    },
+    update(id, data) {
+        return this.execute('put', `/${id}`, data)
+    },
+}
