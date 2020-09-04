0d5d8a25355c019e717ffb713866159e38f86d8e
Mihail Gaberov <mihail.gaberov@gmail.com>
2020-09-04 15:55:53 +0300

    :sparkles: Integrated the voting functionality with the logged in user. Show an warning saying the user should be logged in in order to vote, when not logged in.

--- frontend/src/components/Dashboard.vue
+++ frontend/src/components/Dashboard.vue
@@ -1,14 +1,14 @@
 <template>
   <div id="dashboard">
     <h1 class="h1">Love Pizza, Vote for Pizza</h1>
+    <b-alert :show="error.length === 0 && !isLogged" variant="warning">Please login in order to be able to vote.</b-alert>
     <b-alert :show="error.length > 0" variant="danger">{{ error }}</b-alert>
     <b-alert :show="loading" variant="info">Loading...</b-alert>
-    <div>user data: {{user}}</div>
     <div class="main">
       <div class="positive">
         <b-badge v-if="userRecords" v-show="userRecords.value > 0" variant="success" class="count">{{ userRecords.value }}</b-badge>
         <b-btn v-show="!loading && isLogged" type="submit" variant="success"
-               @click.prevent="addVote('mihail.gaberov@gmail.com', VOTES.UP)">
+               @click.prevent="addVote(loggedInUser.email, VOTES.UP)">
           Like +
         </b-btn>
       </div>
@@ -16,7 +16,7 @@
       <div class="negative">
         <b-badge v-if="userRecords" v-show="userRecords.value <= 0" variant="danger" class="count">{{ userRecords.value }}</b-badge>
         <b-btn v-show="!loading && isLogged" type="submit" variant="danger"
-               @click.prevent="addVote('mihail.gaberov@gmail.com', VOTES.DOWN)">Dislike -
+               @click.prevent="addVote(loggedInUser.email, VOTES.DOWN)">Dislike -
         </b-btn>
       </div>
     </div>
@@ -50,6 +50,16 @@ import api from '@/PizzaVotesApiService';
 
 export default {
   props: { user: Object },
+
+  watch: {
+    user: async function(newVal) {
+      if (newVal) {
+        this.loggedInUser = newVal;
+        this.userRecords = await this.getById(newVal.email)
+      }
+    }
+  },
+
   data() {
     return {
       loading: false,
@@ -59,12 +69,9 @@ export default {
       VOTES: {
         UP: 'UP',
         DOWN: 'DOWN'
-      }
-    };
-  },
-  async created() {
-    this.userRecords = await this.getById('mihail.gaberov@gmail.com')
-    console.log('>>> props', this.$router)
+      },
+      loggedInUser: this.user,
+    }
   },
   methods: {
     async getById(id) {
@@ -75,10 +82,9 @@ export default {
         return await api.getById(id)
       } catch (error) {
         if (error && error.response && error.response.status === 401) {
-          this.error = "Please login in order to be able to vote."
           this.isLogged = false
         } else {
-          this.error = error.message;
+          this.error = error.message
         }
       } finally {
         this.loading = false
@@ -86,7 +92,7 @@ export default {
     },
     async addVote(id, dir) {
       const value = dir === this.VOTES.UP ? 1 : -1
-      const isUpdating = this.userRecords.id === 'mihail.gaberov@gmail.com'
+      const isUpdating = this.userRecords.id === this.loggedInUser.email
       const data = {
         id,
         Value: isUpdating ? this.userRecords.value + value : value,
@@ -104,9 +110,9 @@ export default {
         this.userRecords = await this.getById(id)
       } catch (error) {
         if (error && error.response && error.response.status === 401) {
-          this.error = "Please login in order to be able to vote."
+          this.isLogged = false
         } else {
-          this.error = error.message;
+          this.error = error.message
         }
       }
     }
