6c616ca84494a9deee930ca5e51c0d2ffb4e709e
mihailgaberov <mihail.gaberov@gmail.com>
2020-09-05 11:23:17 +0300

    :sparkles: Finished VotesChart component - show top 10 voters sorted from the biggest to the smallest.

--- frontend/src/components/VotesChart.vue
+++ frontend/src/components/VotesChart.vue
@@ -1,15 +1,24 @@
 <script>
 import { Bar } from 'vue-chartjs'
 
+const TOP_N_VOTERS_TO_SHOW_ON_CHART = 10
+
+// Used to sort by votes value - from bigger to smaller (desc)
+function sortByStartDate(nextUser, currentUser) {
+  const nextVoteValue = nextUser.value
+  const currentVoteValue = currentUser.value
+  return currentVoteValue - nextVoteValue
+}
+
 export default {
   extends: Bar,
   props: { data: Array },
 
   watch: {
-    data: async function(newVal) {
-      // TODO: sort the array to contain only top 10 users with the highest appreciation
-      this.labels = newVal.map(user => user.id);
-      this.values = newVal.map(user => user.value)
+    data: async function (newVal) {
+      const sortedVotes = Array.from(newVal).sort(sortByStartDate).slice(0, TOP_N_VOTERS_TO_SHOW_ON_CHART)
+      this.labels = sortedVotes.map(user => user.id)
+      this.values = sortedVotes.map(user => user.value)
 
       this.renderChart({
         labels: this.labels,
@@ -17,11 +26,20 @@ export default {
           {
             label: 'Pizza Lovers',
             backgroundColor: '#f87979',
-            data: this.values
+            data: this.values,
           }
         ]
+      }, {
+        scales: {
+          yAxes: [{
+            ticks: {
+              stepSize: 1,
+              min: 0,
+              max: this.values[0]
+            }
+          }]
+        }
       })
-
     }
   }
 }
