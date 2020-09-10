4052871c8e61bde7d44ae65fe1bce3566226e810
mihailgaberov <mihail.gaberov@gmail.com>
2020-09-10 23:57:31 +0300

    :white_check_mark: Added e2e tests for the dashboard page before login.

--- frontend/tests/e2e/specs/test.js
+++ frontend/tests/e2e/specs/test.js
@@ -4,5 +4,15 @@ describe('My First Test', () => {
   it('Visits the app root url', () => {
     cy.visit('/')
     cy.contains('h1', 'Love Pizza, Vote for Pizza')
+    cy.contains('.navbar-brand', 'Love Pizza')
+    cy.contains('.nav-link', 'Login')
+    cy.contains('.alert', 'Please login in order to be able to vote.')
+    cy.get('[alt="Love Pizza, Vote for Pizza"]')
+        .should('be.visible')
+        .and(($img) => {
+          expect($img[0].naturalWidth).to.be.greaterThan(0)
+        })
+    cy.get('#bar-chart')
+        .should('be.visible')
   })
 })
