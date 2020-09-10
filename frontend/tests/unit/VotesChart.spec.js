acb560f883009ed317c040e032a55c1b5aa7899f
mihailgaberov <mihail.gaberov@gmail.com>
2020-09-10 23:08:28 +0300

    :white_check_mark: Added unit tests for VotesChart component.

--- frontend/tests/unit/VotesChart.spec.js
+++ frontend/tests/unit/VotesChart.spec.js
@@ -2,8 +2,9 @@ import { render } from '@testing-library/vue'
 import VotesChart from "@/components/VotesChart";
 
 describe('Dashboard.vue', () => {
-    test('container contains canvas', () => {
+    test('container contains canvas that renders a Bar chart', () => {
         const { container } = render(VotesChart)
         expect(container.lastChild.lastChild.tagName).toBe('CANVAS')
+        expect(container.lastChild.lastChild.attributes[0].value).toBe('bar-chart')
     })
 })
