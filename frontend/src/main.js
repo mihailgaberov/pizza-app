5233c9dd04325b0e8485bffee597b6dfb9c11681
Mihail Gaberov <mihail.gaberov@gmail.com>
2020-09-10 15:03:34 +0300

    :fire: Removed redundant store stuff.

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store'
 import BootstrapVue from 'bootstrap-vue'
 import './custom.scss';
 
@@ -11,6 +10,5 @@ Vue.config.productionTip = false
 
 new Vue({
   router,
-  store,
   render: h => h(App)
 }).$mount('#app')
